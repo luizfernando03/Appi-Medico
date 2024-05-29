234ca8cf93645c956922efd4393e74bb34768d53
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-05-29 16:39:40 -0300

    subindo dados da classe Endereco

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -1,4 +1,13 @@
 package med.voll.api.endereco;
 
 public class Endereco {
+
+    private String logradouro;
+    private String bairrro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cicade;
+    private String uf;
+
 }
