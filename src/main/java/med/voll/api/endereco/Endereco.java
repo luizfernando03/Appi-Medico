68a81c74a799e0ae71a3e51b18f99c9e1b470009
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-06-10 13:17:41 -0300

    update

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -1,7 +1,18 @@
 package med.voll.api.endereco;
 
 
+import ch.qos.logback.core.model.processor.TimestampModelHandler;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.awt.font.TextHitInfo;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Endereco {
 
     private String logradouro;
@@ -12,4 +23,12 @@ public class Endereco {
     private String cicade;
     private String uf;
 
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairrro = dados.bairro();
+        this.uf = dados.uf();
+        this.cicade = dados.cidade();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+    }
 }
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -4,8 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 import med.voll.api.endereco.Endereco;
 
-@Table
-@Entity
+@Table(name = "medicos")
+@Entity(name = "Medico")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,4 +23,12 @@ public class Medico {
 
     @Embedded
     private Endereco endereco;
+
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
 }
